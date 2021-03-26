00e353d7e2460ab201433e1bf4131fdf3fc01e7b
student <s1075446@gm.pu.edu.tw>
2021-03-26 16:02:35 +0800

    0326

--- app/src/main/java/tw/edu/pu/csim/s1075446/counter20210319/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1075446/counter20210319/MainActivity.kt
@@ -3,16 +3,34 @@ package tw.edu.pu.csim.s1075446.counter20210319
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    View.OnClickListener, View.OnLongClickListener {
 
     var counter:Int=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv.text=counter.toString()
+
+        txv.setOnClickListener(this)
+        btn3.setOnClickListener(this)
+        txv.setOnLongClickListener(this)
+
+        btnHappy.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(v: View?) {
+                counter= (1..100).random()
+                txv.text = counter.toString()
+
+            }
+
+        })
+
+        img.setOnLongClickListener(this)
     }
 
     fun AddOne(v: View){
@@ -23,5 +41,27 @@ class MainActivity : AppCompatActivity() {
         counter++
         counter++
         txv.text=counter.toString()
+    }
+
+    override fun onClick(v: View?) {
+        if(v==txv||v==btn){
+            counter++
+        }
+        else if(v==btn2){
+            counter +=2
+        }
+        else{
+            counter=0
+        }
+        txv.text=counter.toString()
+    }
+
+    override fun onLongClick(v: View?): Boolean {
+        if(v==txv){counter +=2}
+        else{
+            counter--
+        }
+        txv.text=counter.toString()
+        return true
     }
 }
